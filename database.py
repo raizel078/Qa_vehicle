6a33330e7af7f565a6e02fcbe3ef305f9ad0210c
raizel078 <poudelsunil078@gmail.com>
2026-08-22 12:11:45 -0400

    done for this one

--- database.py
+++ database.py
@@ -1,5 +1,10 @@
 import  sqlite3
 
+from transformers.dynamic_module_utils import resolve_trust_remote_code
+
+from Qa_vehicle.embder import timestamp
+from Qa_vehicle.querry import result
+
 conn = sqlite3.connect('database.db')
 
 def create_table(conn):
@@ -13,3 +18,15 @@ def insert_detection(conn, timestamp, label):
 def get_detection(conn):
     rows = conn.execute('SELECT timestamp, label FROM detection').fetchall()
     return rows
+
+def count_label(conn, target):
+    result = conn.execute(
+        'SELECT COUNT(*) FROM detection WHERE Label=?',(target,)
+    ).fetchone()
+    return result
+
+def lookup(conn, target):
+    result = conn.execute(
+        "SELECT timestamp FROM detection WHERE label = ?", (target,)
+    ).fetchall()
+    return result
--- querry.py
+++ querry.py
@@ -1,6 +1,45 @@
-import  chromadb
+import pickle
+
+import chromadb
+from sentence_transformers import SentenceTransformer
+import spacy
+from ultralytics import YOLO
+from database import conn, count_label , lookup
+
+
+
+clf = pickle.load(open('intent_model.pk1','rb'))
+model = SentenceTransformer('all-MiniLM-L6-v2')
+nlp = spacy.load('en_core_web_sm')
 
 
 query = str(input('Ask me the Question'))
+vector = model.encode([query])
+intent = clf.predict(vector)[0]
+
+class_list = list(YOLO('yolo11n.pt').names.values())
+
+#code to get the details.
+doc = nlp(query)
+
+for i in doc:
+    if i.lemma_ in class_list:
+        target = i.lemma_
+        if intent == 'count':
+            print(count_label(conn, target))
+        elif intent == 'lookup':
+            print(lookup(conn, target))
+
+        elif intent =='fuzzy':
+            client = chromadb.PersistentClient(path='chroma_db')
+            collection = client.get_collection('detections')
+            results = collection.query(
+                query_embeddings=vector.tolist(),
+                n_results=5
+            )
+            print(results['documents'])
+            print(results['metadatas'])
+
+
 
 
